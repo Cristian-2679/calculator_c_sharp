fd69649acb8c9e301951843c039013af3eeb805e
Cristian-2679 <128701111+Cristian-2679@users.noreply.github.com>
2023-09-19 18:23:01 +0200

    Started coding

--- calculator project/Form1.Designer.cs
+++ calculator project/Form1.Designer.cs
@@ -28,13 +28,56 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackgroundImage = global::calculator_project.Properties.Resources.FCEw1bYWUAQfIjs;
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(421, 72);
+            this.panel1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Jokerman", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.LemonChiffon;
+            this.label1.Image = global::calculator_project.Properties.Resources.FCEw1bYWUAQfIjs;
+            this.label1.ImageAlign = System.Drawing.ContentAlignment.TopRight;
+            this.label1.Location = new System.Drawing.Point(12, 75);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(397, 87);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "0";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.Khaki;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(421, 539);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Calcolatrice";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- calculator project/Form1.cs
+++ calculator project/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,6 +16,180 @@ namespace calculator_project
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            makebuttons();
+        }
+
+        public struct btnStruct
+        {
+            public string content;
+            public bool font;
+            public btnStruct(string c, bool f)
+            {
+                this.content = c;
+                this.font = f;
+            }
+        }
+
+        private btnStruct[,] buttons =
+        {
+            { new btnStruct("%", false),new btnStruct("CE", true),new btnStruct("C", true),new btnStruct("<X|", true)},
+            { new btnStruct("1/x", true),new btnStruct("x²", true),new btnStruct("²√x", true),new btnStruct("/", false)},
+            { new btnStruct("7", false),new btnStruct("8", false),new btnStruct("9", false),new btnStruct("*", false)},
+            { new btnStruct("4", false),new btnStruct("5", false),new btnStruct("6", false),new btnStruct("-", false)},
+            { new btnStruct("1", false),new btnStruct("2", false),new btnStruct("3", false),new btnStruct("+", false)},
+            { new btnStruct("+/-", false),new btnStruct("0", false),new btnStruct(",", false),new btnStruct("=", false)}
+        };
+
+        private void makebuttons()
+        {
+            for(int i = 0; i < 6; i++)
+            {
+                for(int j = 0; j < 4; j++)
+                {
+                    Button mybutton= new Button();
+                    mybutton.Text= buttons[i, j].content;
+                    mybutton.Top = 162+ 62*i;
+                    mybutton.Left = 105*j;
+                    mybutton.Width = 105;
+                    mybutton.Height = 62;
+                    mybutton.BackColor = Color.SandyBrown;
+                    mybutton.ForeColor = Color.Wheat;
+                    if (buttons[i, j].font)
+                    {
+                        mybutton.Font = new Font("Unown", 34);
+                    }
+                    else
+                    {
+                        mybutton.Font = new Font("Jokerman", 21);
+                    }
+                    if( j==3 || i == 1 || i == 0)
+                    {
+                        mybutton.BackColor = Color.BurlyWood;
+                        mybutton.ForeColor = Color.Coral;
+                    }
+                    if (i + j == 8)
+                    {
+                        mybutton.BackColor = Color.Coral;
+                        mybutton.ForeColor = Color.BurlyWood;
+                    }
+                    mybutton.Click += Mybutton_Click;
+                    this.Controls.Add(mybutton);
+                }
+            }
+        }
+
+        private void Mybutton_Click(object sender, EventArgs e)
+        {
+            Button h = (Button)sender;
+            string j = h.Text;
+            //System.Windows.Forms.Label lb = label1;
+            switch (j)
+            {
+                case "1":
+                    label1.Text += "1";
+                    break;
+                case "2":
+                    label1.Text += "2";
+                    break;
+                case "3":
+                    label1.Text += "3";
+                    break;
+                case "4":
+                    label1.Text += "4";
+                    break;
+                case "5":
+                    label1.Text += "5";
+                    break;
+                case "6":
+                    label1.Text += "6";
+                    break;
+                case "7":
+                    label1.Text += "7";
+                    break;
+                case "8":
+                    label1.Text += "8";
+                    break;
+                case "9":
+                    label1.Text += "9";
+                    break;
+                case "0":
+                    label1.Text += "0";
+                    break;
+                case "CE":
+                    label1.Text = "";
+                    break;
+                case "C":
+                    break;
+                case "%":
+                    break;
+                case "<X|":
+                    if(label1.Text.Length > 0) 
+                    { 
+                    label1.Text = label1.Text.Remove(label1.Text.Length - 1);
+                    }
+                    break;
+                case "x²":
+                    label1.Text += "²";
+                    break;
+                case "²√x":
+                    label1.Text = "√" + label1.Text;
+                    break;
+                case "1/x":
+                    label1.Text = "1/" + label1.Text;
+                    break;
+                case "/":
+                    label1.Text += "/";
+                    //Next();
+                    break;
+                case "*":
+                    label1.Text += "*";
+                    break;
+                case "+":
+                    label1.Text += "+";
+                    break;
+                case "-":
+                    label1.Text += "-";
+                    break;
+                case "+/-":
+                    if (label1.Text[0] == '+')
+                    {
+                        label1.Text = "-" + label1.Text.Substring(1);
+                    }
+                    else
+                    {
+                        if (label1.Text[0] == '-')
+                        {
+                            label1.Text = "+" + label1.Text.Substring(1);
+                        }
+                        else
+                        {
+                            label1.Text= "-"+label1.Text;
+                        }
+
+                    }
+                    break;
+                case ",":
+                    if(!(label1.Text.Contains(",") || label1.Text.Length==0))
+                    {
+                        label1.Text += ",";
+                    }
+                    break;
+                case "=":
+                    break;
+            }
+            if (label1.Text.Length > 1 && label1.Text[0] == '0' && label1.Text[1] != ',')
+            {
+                label1.Text = label1.Text.Substring(1);
+            }
+        }
+
+        private void Next()
+        {
+            throw new NotImplementedException();
         }
     }
 }
